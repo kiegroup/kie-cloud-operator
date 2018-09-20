997258f51997b45283637a2caa4d034cfdc7916f
Babak Mozaffari <bmozaffa@redhat.com>
2018-09-19 17:05:22 -0700

    [KIECLOUD-7] Provide unit tests
    Signed-off-by: Babak Mozaffari <bmozaffa@redhat.com>

--- internal/pkg/defaults/defaults_test.go
+++ internal/pkg/defaults/defaults_test.go
@@ -7,14 +7,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCommonEnvironmentDefaults(t *testing.T) {
+func TestConsoleEnvironmentDefaults(t *testing.T) {
 	defaults := ConsoleEnvironmentDefaults()
-	logrus.Infof("Loaded common defaults as %v", defaults)
-	assert.True(t, true)
+	logrus.Debugf("Loaded common defaults as %v", defaults)
+	assert.Equal(t, defaults["SSO_OPENIDCONNECT_DEPLOYMENTS"], "ROOT.war", "Expected ROOT.war as the value for SSO_OPENIDCONNECT_DEPLOYMENTS")
+	assert.Equal(t, defaults["KIE_MAVEN_USER"], "mavenUser", "Expected mavenUser as the value for KIE_MAVEN_USER")
 }
 
 func TestServerEnvironmentDefaults(t *testing.T) {
 	defaults := ServerEnvironmentDefaults()
-	logrus.Infof("Loaded server defaults as %v", defaults)
-	assert.True(t, true)
+	logrus.Debugf("Loaded server defaults as %v", defaults)
+	assert.Equal(t, defaults["SSO_OPENIDCONNECT_DEPLOYMENTS"], "ROOT.war", "Expected ROOT.war as the value for SSO_OPENIDCONNECT_DEPLOYMENTS")
+	assert.Equal(t, defaults["RHPAMCENTR_MAVEN_REPO_USERNAME"], "mavenUser", "Expected mavenUser as the value for RHPAMCENTR_MAVEN_REPO_USERNAME")
 }
