97843246d030fbad5fd5d7f699d09ad9e805b847
tchughesiv <tchughesiv@gmail.com>
2019-03-07 13:35:55 -0600

    keystore secret selection
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- pkg/controller/kieapp/kieapp_controller_test.go
+++ pkg/controller/kieapp/kieapp_controller_test.go
@@ -308,6 +308,13 @@ func TestGenerateSecret(t *testing.T) {
 		},
 		Spec: v1.KieAppSpec{
 			Environment: "rhpam-trial",
+			Objects: v1.KieAppObjects{
+				Servers: []v1.KieServerSet{
+					{Deployments: defaults.Pint(3)},
+					{Name: "testing", Deployments: defaults.Pint(4)},
+					{Deployments: defaults.Pint(2)},
+				},
+			},
 		},
 	}
 	env, err := defaults.GetEnvironment(cr, test.MockService())
@@ -324,8 +331,77 @@ func TestGenerateSecret(t *testing.T) {
 	env, _, err = reconciler.newEnv(cr)
 	assert.Nil(t, err, "Error creating a new environment")
 	assert.Len(t, env.Console.Secrets, 1, "One secret should be generated for the trial workbench")
-	assert.Len(t, env.Servers[0].Secrets, 1, "One secret should be generated for the trial workbench")
-	assert.Equal(t, "test-kieserver-app-secret", env.Servers[0].Secrets[0].Name)
+	for _, server := range env.Servers {
+		assert.Len(t, server.Secrets, 1, "One secret should be generated for each trial kieserver")
+		secretName := fmt.Sprintf(constants.KeystoreSecret, server.DeploymentConfigs[0].Name)
+		assert.Equal(t, secretName, server.Secrets[0].Name)
+		for _, volume := range server.DeploymentConfigs[0].Spec.Template.Spec.Volumes {
+			if volume.Secret != nil {
+				assert.Equal(t, secretName, volume.Secret.SecretName)
+			}
+		}
+	}
+}
+
+func TestSpecifySecret(t *testing.T) {
+	cr := &v1.KieApp{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test",
+		},
+		Spec: v1.KieAppSpec{
+			Environment: "rhpam-trial",
+			Objects: v1.KieAppObjects{
+				Console: v1.SecuredKieAppObject{
+					KieAppObject: v1.KieAppObject{
+						KeystoreSecret: "console-ks-secret",
+					},
+				},
+				Servers: []v1.KieServerSet{
+					{
+						SecuredKieAppObject: v1.SecuredKieAppObject{
+							KieAppObject: v1.KieAppObject{
+								KeystoreSecret: "server-ks-secret",
+							},
+						},
+					},
+				},
+				SmartRouter: v1.KieAppObject{
+					KeystoreSecret: "smartrouter-ks-secret",
+				},
+			},
+		},
+	}
+	env, err := defaults.GetEnvironment(cr, test.MockService())
+	assert.Nil(t, err, "Error getting a new environment")
+	assert.Len(t, env.Console.Secrets, 0, "No secret is available when reading the trial workbench from yaml files")
+
+	scheme, err := v1.SchemeBuilder.Build()
+	assert.Nil(t, err, "Failed to get scheme")
+	mockService := test.MockService()
+	mockService.GetSchemeFunc = func() *runtime.Scheme {
+		return scheme
+	}
+	reconciler := &Reconciler{mockService}
+	env, _, err = reconciler.newEnv(cr)
+	assert.Nil(t, err, "Error creating a new environment")
+	assert.Len(t, env.Console.Secrets, 0, "Zero secrets should be generated for the trial workbench")
+	assert.Len(t, env.Servers[0].Secrets, 0, "Zero secrets should be generated for the trial kieserver")
+	assert.Len(t, env.SmartRouter.Secrets, 0, "Zero secrets should be generated for the smartrouter")
+	for _, volume := range env.Console.DeploymentConfigs[0].Spec.Template.Spec.Volumes {
+		if volume.Secret != nil {
+			assert.Equal(t, cr.Spec.Objects.Console.KeystoreSecret, volume.Secret.SecretName)
+		}
+	}
+	for _, volume := range env.Servers[0].DeploymentConfigs[0].Spec.Template.Spec.Volumes {
+		if volume.Secret != nil {
+			assert.Equal(t, cr.Spec.Objects.Servers[0].KeystoreSecret, volume.Secret.SecretName)
+		}
+	}
+	for _, volume := range env.SmartRouter.DeploymentConfigs[0].Spec.Template.Spec.Volumes {
+		if volume.Secret != nil {
+			assert.Equal(t, cr.Spec.Objects.SmartRouter.KeystoreSecret, volume.Secret.SecretName)
+		}
+	}
 }
 
 func TestConsoleHost(t *testing.T) {
