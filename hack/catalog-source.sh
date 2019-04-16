869db128d785f41b9afe7ad57b9c952ada3c7741
tchughesiv <tchughesiv@gmail.com>
2019-04-16 13:50:32 -0500

    cherry-pick omps enhancements
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- hack/catalog-source.sh
+++ hack/catalog-source.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+VERSION="1.0.1"
+
 if [[ -z ${1} ]]; then
     CATALOG_NS="operator-lifecycle-manager"
 else
     CATALOG_NS=${1}
 fi
 
-CSV=`cat deploy/catalog_resources/redhat/businessautomation-operator.1.0.1.clusterserviceversion.yaml | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
+CSV=`cat deploy/catalog_resources/redhat/${VERSION}/businessautomation-operator.${VERSION}.clusterserviceversion.yaml | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
 CRD=`cat deploy/crds/kieapp.crd.yaml | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
-PKG=`cat deploy/catalog_resources/redhat/businessautomation.1.0.1.package.yaml | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
+PKG=`cat deploy/catalog_resources/redhat/${VERSION}/businessautomation.${VERSION}.package.yaml | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
 
 cat << EOF > deploy/catalog_resources/redhat/catalog-source.yaml
 apiVersion: v1
