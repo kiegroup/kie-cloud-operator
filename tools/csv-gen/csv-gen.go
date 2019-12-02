8d246ac54f4604d594b5325015257e56bb283b19
tchughesiv <tchughesiv@gmail.com>
2019-12-02 14:47:01 -0600

    7.6.1 / 1.3.1 version
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- tools/csv-gen/csv-gen.go
+++ tools/csv-gen/csv-gen.go
@@ -50,7 +50,7 @@ var (
 			Registry:     "registry.redhat.io",
 			Context:      "rhpam-" + major,
 			ImageName:    "rhpam-rhel8-operator",
-			Tag:          constants.VersionConstants[constants.CurrentVersion].ImageTag,
+			Tag:          constants.CurrentVersion,
 		},
 	}
 )
