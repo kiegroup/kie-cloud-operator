763c4d1dfcbca0eb0a99f4bfdafabb53baa8c439
tchughesiv <tchughesiv@gmail.com>
2020-01-27 10:07:38 -0600

    add CI that catches prior version config changes
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- hack/go-test.sh
+++ hack/go-test.sh
@@ -8,19 +8,20 @@ if [[ -z ${CI} ]]; then
     ./hack/go-vet.sh
     ./hack/go-fmt.sh
 else
-    #JOB_SPEC='{"type":"presubmit","job":"pull-ci-kiegroup-kie-cloud-operator-master-unit","buildid":"659","prowjobid":"c581cca6-3ee0-11ea-bc44-0a58ac103085","refs":{"org":"kiegroup","repo":"kie-cloud-operator","repo_link":"https://github.com/kiegroup/kie-cloud-operator","base_ref":"master","base_sha":"6c7e0915536125ba931b9df30c349c008f142a8b","base_link":"https://github.com/kiegroup/kie-cloud-operator/commit/6c7e0915536125ba931b9df30c349c008f142a8b","pulls":[{"number":344,"author":"tchughesiv","sha":"253a9f2c35747295c0a35df3afa5b599de9f1e6f","link":"https://github.com/kiegroup/kie-cloud-operator/pull/344","commit_link":"https://github.com/kiegroup/kie-cloud-operator/pull/344/commits/253a9f2c35747295c0a35df3afa5b599de9f1e6f","author_link":"https://github.com/tchughesiv"}]}}'
-    BASE_REF=$(echo ${JOB_SPEC} | python -c "import sys, json; print(json.load(sys.stdin)['refs']['base_ref'])")
+    JOB_SPEC='{"type":"presubmit","job":"pull-ci-kiegroup-kie-cloud-operator-master-unit","buildid":"659","prowjobid":"c581cca6-3ee0-11ea-bc44-0a58ac103085","refs":{"org":"kiegroup","repo":"kie-cloud-operator","repo_link":"https://github.com/kiegroup/kie-cloud-operator","base_ref":"master","base_sha":"6c7e0915536125ba931b9df30c349c008f142a8b","base_link":"https://github.com/kiegroup/kie-cloud-operator/commit/6c7e0915536125ba931b9df30c349c008f142a8b","pulls":[{"number":344,"author":"tchughesiv","sha":"3add571677bbe47d10f6aa679b3f9d9cd45a850b","link":"https://github.com/kiegroup/kie-cloud-operator/pull/344","commit_link":"https://github.com/kiegroup/kie-cloud-operator/pull/344/commits/253a9f2c35747295c0a35df3afa5b599de9f1e6f","author_link":"https://github.com/tchughesiv"}]}}'
+    BASE_SHA=$(echo ${JOB_SPEC} | python -c "import sys, json; print(json.load(sys.stdin)['refs']['base_sha'])")
+    # SHA=$(echo ${JOB_SPEC} | python -c "import sys, json; print(json.load(sys.stdin)['refs']['pulls'][0]['sha'])")
     REPO_LINK=$(echo ${JOB_SPEC} | python -c "import sys, json; print(json.load(sys.stdin)['refs']['repo_link'])")
-    if [[ -z ${BASE_REF} ]]; then
-        echo "base_ref not found, can't execute diff"
+    if [[ -z ${BASE_SHA} ]]; then
+        echo "base_sha not found, can't execute diff"
         exit 1
     else
-        pwd -P
         git remote add origin ${REPO_LINK}
-        git pull origin
+        git fetch origin ${BASE_SHA}
         git show-ref
+        # git checkout ${SHA}
         VERSION=$(go run getversion.go -product)
-        RESULT=$(git diff --name-only origin/${BASE_REF} | grep "^config/" | grep -v "^config/${VERSION}")
+        RESULT=$(git diff --name-only ${BASE_SHA} | grep "^config/" | grep -v "^config/${VERSION}")
         if [[ ${RESULT} ]]; then
             echo "\nDetected changes to an older version's config file(s). Current version changes are only allowed in config/${VERSION}."
             echo "Undo changes to the following files -"
