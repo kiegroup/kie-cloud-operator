b486b0dcc7da2e899d1c6a259d58a036bd60c326
tchughesiv <tchughesiv@gmail.com>
2020-07-02 10:26:58 -0500

    fix multi-stage for osbs
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -3,11 +3,11 @@
 source ./hack/go-mod-env.sh
 
 REPO=https://github.com/kiegroup/kie-cloud-operator
-BRANCH=$(go run getversion.go)
+BRANCH=$(go run getversion.go -operator)
 REGISTRY=quay.io/kiegroup
 IMAGE=kie-cloud-operator
-TAR=modules/builder/${IMAGE}.tar.gz
-URL=${REPO}/archive/${BRANCH}.tar.gz
+TAR=${BRANCH}.tar.gz
+URL=${REPO}/archive/${TAR}
 CFLAGS="docker"
 
 if [[ -z ${CI} ]]; then
@@ -16,9 +16,16 @@ fi
 
 ./hack/go-gen.sh
 
+echo
+echo Will build console first:
+echo
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -mod=vendor -a -o build/_output/bin/console-cr-form ./cmd/ui
+echo
+
 if [[ -z ${CI} ]]; then
     echo Now building operator:
     echo
+    operator-sdk build --go-build-args -mod=vendor ${REGISTRY}/${IMAGE}:${BRANCH}
     if [[ ${1} == "rhel" ]]; then
         if [[ ${LOCAL} != true ]]; then
             CFLAGS="osbs"
@@ -27,20 +34,14 @@ if [[ -z ${CI} ]]; then
             fi
         fi
         wget -q ${URL} -O ${TAR}
-        echo ${CFLAGS}
-        cekit --verbose --redhat build \
-           ${CFLAGS}
+        MD5=$(md5sum ${TAR} | awk {'print $1'})
         rm ${TAR}
-    else
-        echo
-        echo Will build console first:
-        echo
-        CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -mod=vendor -a -o build/_output/bin/console-cr-form ./cmd/ui
-        echo
 
-        operator-sdk build --go-build-args -mod=vendor ${REGISTRY}/${IMAGE}:${BRANCH}
+        echo ${CFLAGS}
+        cekit --redhat build \
+            --overrides "{'artifacts': [{'name': 'kie-cloud-operator.tar.gz', 'md5': '${MD5}', 'url': '${URL}'}]}" \
+            ${CFLAGS}
     fi
 else
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -mod=vendor -a -o build/_output/bin/console-cr-form ./cmd/ui
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -mod=vendor -a -o build/_output/bin/kie-cloud-operator ./cmd/manager
 fi
