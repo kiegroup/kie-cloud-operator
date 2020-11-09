9bf1089ad3e23f8deddf735712517d256b52a0b9
tchughesiv <tchughesiv@gmail.com>
2020-11-09 15:48:40 -0600

    generate csv for 7.9.0 bug fix
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- hack/go-csv.sh
+++ hack/go-csv.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 source ./hack/go-mod-env.sh
+VERSION=$(go run getversion.go)
 
-go run ./tools/csv-gen/csv-gen.go
-operator-sdk bundle validate deploy/olm-catalog/dev/$(go run getversion.go)
-operator-sdk bundle validate deploy/olm-catalog/prod/$(go run getversion.go)
+#go run ./tools/csv-gen/csv-gen.go
+go run ./tools/csv-gen/csv-gen.go -version 7.9.0-2 -replaces ${VERSION}
+
+#operator-sdk bundle validate deploy/olm-catalog/dev/${VERSION}
+#operator-sdk bundle validate deploy/olm-catalog/prod/${VERSION}
+operator-sdk bundle validate deploy/olm-catalog/dev/7.9.0-2
+operator-sdk bundle validate deploy/olm-catalog/prod/7.9.0-2
