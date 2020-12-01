5c50a5fda6be574e954239c4e5c3265f5623613a
tchughesiv <tchughesiv@gmail.com>
2020-12-01 09:46:12 -0600

    [KIECLOUD-474] increase amq version to 7.8
    Signed-off-by: tchughesiv <tchughesiv@gmail.com>

--- tools/csv-gen/csv-gen.go
+++ tools/csv-gen/csv-gen.go
@@ -342,8 +342,8 @@ func main() {
 		relatedImages = addRefRelatedImages(constants.PostgreSQL10ImageURL, constants.PostgreSQL10Component, imageRef, relatedImages)
 		relatedImages = addRefRelatedImages(constants.Datagrid73ImageURL15, constants.Datagrid73Component, imageRef, relatedImages)
 		relatedImages = addRefRelatedImages(constants.Datagrid73ImageURL16, constants.Datagrid73Component, imageRef, relatedImages)
-		relatedImages = addRefRelatedImages(constants.Broker76ImageURL, constants.BrokerComponent, imageRef, relatedImages)
 		relatedImages = addRefRelatedImages(constants.Broker77ImageURL, constants.BrokerComponent, imageRef, relatedImages)
+		relatedImages = addRefRelatedImages(constants.Broker78ImageURL, constants.BrokerComponent, imageRef, relatedImages)
 
 		if logs.GetBoolEnv("DIGESTS") {
 			// use stage registry for current release image digest population
