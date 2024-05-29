1f48c0364f3dc7275639767e49a9abbc5fb0998c
Abhishek Kumar <19465051+akumar074@users.noreply.github.com>
2024-05-29 11:52:51 +0530

    RHPAM-4951 : Rebuild BAMOE 8.0.5 Operator Bundle CVE-2024-33599 (#962)

--- version/version.go
+++ version/version.go
@@ -8,9 +8,9 @@ var (
 	// Version - current version
 	Version = constants.CurrentVersion
 	// CsvVersion - csv release
-	CsvVersion = Version + "-1"
+	CsvVersion = Version + "-2"
 	// PriorVersion - prior version
 	PriorVersion = constants.CurrentVersion
 	// CsvPriorVersion - prior csv release
-	CsvPriorVersion = PriorVersion + "-3"
+	CsvPriorVersion = PriorVersion + "-1"
 )
