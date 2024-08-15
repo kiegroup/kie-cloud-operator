a983973e60dc04d058be670e6d42184544adfa36
Abhishek Kumar <akumar074@gmail.com>
2024-08-14 22:26:18 -0700

    Prepare BAMOE Operator 8.0.6

--- pkg/controller/kieapp/constants/constants.go
+++ pkg/controller/kieapp/constants/constants.go
@@ -13,9 +13,9 @@ var Ocp4Versions = []string{"4.14", "4.13", "4.12"}
 
 const (
 	// CurrentVersion product version supported
-	CurrentVersion = "8.0.5"
+	CurrentVersion = "8.0.6"
 	// PriorVersion product version supported
-	PriorVersion = "8.0.4"
+	PriorVersion = "8.0.5"
 )
 
 // SupportedVersions - product versions this operator supports
