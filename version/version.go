5b22ec1f193a7324ac47e9afc5af5a9ed471333d
Roberto Oliveira <rguimara@redhat.com>
2024-11-27 12:42:46 -0500

    Update RHPAM bundle for CVE respin (CVE-2024-10963)

--- version/version.go
+++ version/version.go
@@ -8,9 +8,9 @@ var (
 	// Version - current version
 	Version = constants.CurrentVersion
 	// CsvVersion - csv release
-	CsvVersion = Version + "-8"
+	CsvVersion = Version + "-9"
 	// PriorVersion - prior version
 	PriorVersion = constants.CurrentVersion
 	// CsvPriorVersion - prior csv release
-	CsvPriorVersion = PriorVersion + "-7"
+	CsvPriorVersion = PriorVersion + "-8"
 )
