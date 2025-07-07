6a4ff02df3e77fac7d9005cfb500c712272cc5e1
Saumya Singh <sausingh@redhat.com>
2025-07-07 11:09:23 +0530

    Update RHPAM bundle for CVE respin

--- version/version.go
+++ version/version.go
@@ -8,9 +8,9 @@ var (
 	// Version - current version
 	Version = constants.CurrentVersion
 	// CsvVersion - csv release
-	CsvVersion = Version + "-13"
+	CsvVersion = Version + "-14"
 	// PriorVersion - prior version
 	PriorVersion = constants.CurrentVersion
 	// CsvPriorVersion - prior csv release
-	CsvPriorVersion = PriorVersion + "-12"
+	CsvPriorVersion = PriorVersion + "-13"
 )
