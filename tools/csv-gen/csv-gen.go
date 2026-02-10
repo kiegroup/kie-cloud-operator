9c4553ed3b2a7f9505bfc02cf602f637f5c50795
Abhishek Kumar <akumar074@gmail.com>
2026-02-10 12:33:21 +0530

    Revert KieApp DeploymentConfig

--- tools/csv-gen/csv-gen.go
+++ tools/csv-gen/csv-gen.go
@@ -21,6 +21,7 @@ import (
 	"github.com/kiegroup/kie-cloud-operator/pkg/controller/kieapp/defaults"
 	"github.com/kiegroup/kie-cloud-operator/tools/util"
 	"github.com/kiegroup/kie-cloud-operator/version"
+	oappsv1 "github.com/openshift/api/apps/v1"
 	buildv1 "github.com/openshift/api/build/v1"
 	oimagev1 "github.com/openshift/api/image/v1"
 	routev1 "github.com/openshift/api/route/v1"
@@ -225,8 +226,8 @@ func main() {
 				Name:        "kieapps." + api.SchemeGroupVersion.Group,
 				Resources: []csvv1.APIResourceReference{
 					{
-						Kind:    "Deployment",
-						Version: appsv1.SchemeGroupVersion.String(),
+						Kind:    "DeploymentConfig",
+						Version: oappsv1.GroupVersion.String(),
 					},
 					{
 						Kind:    "StatefulSet",
