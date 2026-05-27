d6599b2da44cf686856c0b403ae5a4a07e9d6232
Jakub Schwan <jschwan@redhat.com>
2026-05-27 16:42:09 +0200

    Revert "Update RHPAM bundle for CVE respin ()"
    This reverts commit beea00f475728d1cd0cf00bf316a42ec1b8ca790.

--- version/version.go
+++ version/version.go
@@ -8,9 +8,9 @@ var (
 	// Version - current version
 	Version = constants.CurrentVersion
 	// CsvVersion - csv release
-	CsvVersion = Version + "-25"
+	CsvVersion = Version + "-24"
 	// PriorVersion - prior version
 	PriorVersion = constants.CurrentVersion
 	// CsvPriorVersion - prior csv release
-	CsvPriorVersion = PriorVersion + "-24"
+	CsvPriorVersion = PriorVersion + "-23"
 )
